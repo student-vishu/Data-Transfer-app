d22a3ce73529ffafbecf90529049bc285adb1192
student-vishu <gandhivishesh222@gmail.com>
2025-05-16 13:52:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const path = require('path')
+const { Server } = require('socket.io')
+const cors = require('cors')
+
+const app = express()
+
+const PORT = process.env.PORT || 8000
+const CORS_ORIGIN = process.env.CORS_ORIGIN || '*'
+
+app.use(cors({
+    origin: CORS_ORIGIN
+}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: CORS_ORIGIN
+    }
+})
+
+//socketIo
+io.on('connection', (socket) => {
+    socket.on('formData', (data) => {
+        // console.log(data);
+        io.emit('display-data', data)
+    })
+
+})
+
+// app.get('/',(req,res)=>{
+//     // res.sendFile()
+// })
+server.listen(PORT, () => { console.log(`Server is running on PORT:${PORT}`); })
